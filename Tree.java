85f8f14150c6db324da1c86bee3a45c0dd29526c
greelyll <greelyll@mail.uc.edu>
2021-03-10 01:45:19 +0000

    Initial commit

---
+++ Tree.java
@@ -0,0 +1,70 @@
+class Tree {
+
+  private String treeName;
+  private String treeType;
+  private boolean leavesFall;
+  private String leafColor;
+
+  Tree() {
+          treeName = "";
+          treeType = "";
+          leavesFall = false;
+          leafColor = "";
+  }
+
+  Tree(String atreeName, String atreeType, boolean aleavesFall, String leafColor) {
+    treeName = atreeName;
+    treeType = atreeType;
+    leavesFall = aleavesFall;
+    leafColor = aleafColor;
+  }
+
+  void settreeName(String thetreeName) {
+              treeName = thetreeName;
+  }
+
+  void settreeType(String thetreeType) {
+              treeType = thetreeType;
+  }
+
+  void setleavesFall(boolean theleavesFall) {
+                leavesFall = theleavesFall;
+  }
+
+  void setleafColor(String theleafColor) {
+                leafColor = theleafColor;
+  }
+
+  String gettreeName() {
+    return treeName;
+  }
+
+  String gettreeType() {
+    return treeType;
+  }
+
+  boolean getleavesFall() {
+    return leaveFall;
+  }
+
+  String getleafColor() {
+    return leafColor;
+  }
+
+  if (leavesFall = true) {
+    System.out.println("does");
+  }
+  else {
+    System.out.println("does not");
+  }
+  void print() {
+              System.out.println();
+              System.out.println("This is a" + treeName + "tree." + "It is a" + treeType + "and its leaves are currently" + leafColor + "." + "It" +   
+              boolean leavesFall = false if (leavesFall = true) {
+    System.out.println("does");
+  }
+  else {
+    System.out.println("does not");
+  } + "lose its leaves for the winter.");
+  }
+}
